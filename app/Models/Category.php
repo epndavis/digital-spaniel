85fa38c46b04678329edb82a3c23eb30e725f5bf
epndavis <47059262+epndavis@users.noreply.github.com>
2020-09-16 22:19:49 +0100

    chore(seeder): Create category seeder - Add project relationship to category model

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     use HasFactory;
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function projects()
+    {
+        return $this->belongsToMany(Project::class);
+    }
 }
