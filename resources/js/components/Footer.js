ec88a5657099809ccd74f5c2a1829ae9e8852740
epndavis <47059262+epndavis@users.noreply.github.com>
2020-09-17 21:57:13 +0100

    style(homepage): Add grid and link stylings

--- resources/js/components/Footer.js
+++ resources/js/components/Footer.js
@@ -7,44 +7,64 @@ class Footer extends React.Component {
             <footer class="container-fluid bg-light">
                 <div class="container">
                     <div class="row footer-content">
-                        <div class="col-6">
+                        <div class="col-12 col-sm-6 mb-3">
                             <div>
                                 <h1>
                                     We're a brands <br />
                                     <span>best friend</span>
                                 </h1>
 
-                                <Link>Let's talk</Link>
+                                <Link class="link-primary">Let's talk</Link>
                             </div>
                         </div>
 
-                        <div class="col-6 links-container">
+                        <div class="col-12 col-sm-6 links-container">
                             <div class="row">
                                 <div class="col-12 col-md-6 col-lg-4">
                                     <h3>
                                         Explore
                                     </h3>
 
-                                    <Link>Services</Link>
+                                    <ul class="list-group">
+                                        <li>
+                                            <Link class="link-secondary">Services</Link>
+                                        </li>
 
-                                    <Link>Work</Link>
+                                        <li>
+                                            <Link class="link-secondary">Work</Link>
+                                        </li>
 
-                                    <Link>About</Link>
-                                    
-                                    <Link>Blog</Link>
+                                        <li>
+                                            <Link class="link-secondary">About</Link>
+                                        </li>
 
-                                    <Link>Careers</Link>
+                                        <li>
+                                            <Link class="link-secondary">Blog</Link>
+                                        </li>
+
+                                        <li>
+                                            <Link class="link-secondary">Careers</Link>
+                                        </li>
+                                    </ul>
                                 </div>
                                 <div class="col-12 col-md-6 col-lg-4">
                                     <h3>
                                         Services
                                     </h3>
 
-                                    <Link>Brand</Link>
+                                    <ul class="list-group">
+                                        <li>
+                                            <Link class="link-secondary">Brand</Link>
+                                        </li>
 
-                                    <Link>Development</Link>
+                                        <li>
+                                            <Link class="link-secondary">Development</Link>
+                                        </li>
 
-                                    <Link>Marketing</Link>
+                                        <li>
+                                            <Link class="link-secondary">Marketing</Link>
+                                        </li>
+                                    </ul>
                                 </div>
                                 <div class="col-12 col-md-6 col-lg-4">
                                     <h3>
